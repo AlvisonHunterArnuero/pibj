efbd8ba001a1997f0c1539a67be5db89637cf67c
AlvisonHunterArnuero <alvison@gmail.com>
2023-05-05 12:54:20 -0600

    Basic Init config files

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+	const log = 78;
+	return (
+		<>
+			<div className="bg-black">
+				<h1 className="bold text-red-400">WELCOME {log}</h1>
+			</div>
+		</>
+	);
+}
+
+export default App;
