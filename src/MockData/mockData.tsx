e397ad3959a6f48a4fc8d7669cf21fc8aa4acb51
AlvisonHunterArnuero <alvison@gmail.com>
2023-05-16 18:58:05 -0600

    Updated MockData Structure based on type

--- src/MockData/mockData.tsx
+++ src/MockData/mockData.tsx
@@ -1,50 +1,48 @@
 import { Link } from 'react-router-dom';
 import { IdentificationIcon } from '../components/IdentificationIcon';
 import { OriginalProps, TableData } from './Types';
-import { CellContext } from '@tanstack/react-table';
-import {
-	JSXElementConstructor,
-	ReactElement,
-	ReactFragment,
-	ReactNode,
-	ReactPortal,
-} from 'react';
 const tblMockData: TableData[] = [
 	{
 		fullName: 'Jorge Luis Ortiz Solano',
 		birthDate: '1981-01-01T00:00:00.000-06:00',
 		address: 'Barrio Ricardo Morales Aviles',
 		gender: 'Male',
+		city: 'Jinotepe',
 		department: 'Carazo',
 		phone: '88241651',
 		maritalStatus: 'Married',
 		baptized: true,
 		memberSince: '2020-02-01T00:00:00.000-06:00',
 		churchRole: 'Servidor',
+		mentor: 'Pastor Roman',
 	},
 	{
 		fullName: 'Onice Esmeralda Acevedo Castillo',
 		birthDate: '1992-10-21T00:00:00.000-06:00',
 		address: 'Praderas de Xilotepelt',
 		gender: 'Female',
+		city: 'Jinotepe',
 		department: 'Carazo',
 		phone: '88888888',
 		maritalStatus: 'Married',
 		baptized: true,
 		memberSince: '2017-03-03T00:00:00.000-06:00',
 		churchRole: 'Directora de Alabanza',
+		mentor: 'Pastor Roman',
 	},
 	{
 		fullName: 'Alvison Hunter Arnuero',
 		birthDate: '1966-02-09T00:00:00.000-06:00',
 		address: 'Residencial Praderas de Xilotepelt Casa G2',
 		gender: 'Male',
+		city: 'Jinotepe',
 		department: 'Carazo',
 		phone: '88638751',
 		maritalStatus: 'Married',
 		baptized: true,
 		memberSince: '2017-06-06T00:00:00.000-06:00',
 		churchRole: 'Ministro de Alabanza',
+		mentor: 'Pastor Roman',
 	},
 ];
 
